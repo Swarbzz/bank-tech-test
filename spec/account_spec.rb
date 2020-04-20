3bf454778759381b8a22c4af367dd98b2356bd12
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-20 11:05:28 +0100

    passing test for withdrawing money from account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -16,6 +16,14 @@ describe Account do
       expect(account.balance).to eq 5.00
       end
     end
+
+    describe "#withdraw" do
+      it "can withdraw money from an account" do
+        account.deposit(5.00)
+        account.withdraw(2.50)
+        expect(account.balance).to eq 2.50
+      end
+    end
   end
-  
+
 end
